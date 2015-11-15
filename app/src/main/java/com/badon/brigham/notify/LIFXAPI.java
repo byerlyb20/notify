9d4b8645fa7f9228f6eeb00f5c63ecdefa593a16
Brigham <byerlybrigham@gmail.com>
2015-11-15 13:56:59 -0500

    2.2 Release

--- app/src/main/java/com/badon/brigham/notify/LIFXAPI.java
+++ app/src/main/java/com/badon/brigham/notify/LIFXAPI.java
@@ -9,28 +9,29 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class LIFXAPI {
 
+    private final String TAG = "LIFXAPI";
     String apiKey;
 
-    public LIFXAPI(String key){
+    public LIFXAPI(String key) {
         apiKey = key;
     }
 
     public void breath(String selector, String color, int cycles) {
         try {
-            Log.i("LIFXAPI", "LIFX API recieved request, trying");
+            Log.i(TAG, "LIFX API recieved request, trying");
             URL obj = new URL("https://api.lifx.com/v1.0-beta1/lights/all/effects/breathe");
             HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
             conn.setRequestProperty("Authorization", " Bearer " + apiKey);
             conn.setRequestMethod("POST");
-            String urlParameters = "selector=" + selector +"&color=" + color + "&cycles=" + cycles;
+            String urlParameters = "selector=" + selector + "&color=" + color + "&cycles=" + cycles;
             conn.setDoOutput(true);
             DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
             wr.writeBytes(urlParameters);
             wr.flush();
             wr.close();
-            Log.i("LIFXAPI", "LIFX API finished with response " + conn.getResponseCode());
+            Log.i(TAG, "LIFX API finished with response " + conn.getResponseCode());
         } catch (Exception e) {
-            Log.i("LIFXAPI", "An exception occurred when trying to use API: " + e);
+            e.printStackTrace();
         }
     }
 }
